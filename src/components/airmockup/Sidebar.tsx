3e7d6483be2f052f412ad5bcf235435cd5f62122
Kang-min Kim <rkdals0203@gmail.com>
2026-04-07 21:48:19 +0900

    Hide sidebar scrollbar, make content area flex-shrink with hidden scrollbar

--- src/components/airmockup/Sidebar.tsx
+++ src/components/airmockup/Sidebar.tsx
@@ -30,7 +30,7 @@ const Sidebar = ({
       initial={{ x: -20, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ duration: 0.4, ease: "easeOut" }}
-      className="w-80 min-w-[320px] h-screen flex flex-col border-r border-border bg-card overflow-y-auto"
+      className="w-80 min-w-[320px] h-screen flex flex-col border-r border-border bg-card overflow-hidden"
     >
       <div className="p-6 pb-4">
         <div className="flex items-center gap-2.5 mb-1">
@@ -45,7 +45,7 @@ const Sidebar = ({
 
       <Separator />
 
-      <div className="flex-1 p-6 space-y-6">
+      <div className="flex-1 min-h-0 p-6 space-y-4 overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
         <ImageDropzone imageUrl={imageUrl} onImageUpload={onImageUpload} />
         <Separator />
         <MagicLink sessionCode={sessionCode} isConnected={isConnected} />
@@ -53,7 +53,7 @@ const Sidebar = ({
         <RotationSliders rotation={rotation} onChange={onRotationChange} />
       </div>
 
-      <div className="p-6 pt-0">
+      <div className="shrink-0 p-6 pt-4">
         <Button onClick={onExport} className="w-full gap-2" size="lg">
           <Download className="w-4 h-4" />
           Capture Current View
